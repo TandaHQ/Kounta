a637121f9bb99f9e922d44e26813e909a014a87c
Samuel Richardson <sam@richardson.co.nz>
2013-09-05 08:33:08 +0000

    Fix tests.

--- spec/kounta/price_list_product.rb
+++ spec/kounta/price_list_product.rb
@@ -6,16 +6,16 @@ describe Kounta::PriceList do
 		@price_list_product = Kounta::Company.new.price_list(236).price_list_products.first
 	end
 
-	it "should have an id" do
-		@price_list_product.id.should be(5510)
+	it "should have a product id" do
+		@price_list_product.product_id.should be(1)
 	end
 
 	it "should have a name" do
-		@price_list_product.name.should eq("Cappuccino")
+		@price_list_product.name.should eq("Test")
 	end
 
 	it "should have a unit price" do
-		@price_list_product.unit_price.should be(1.25)
+		@price_list_product.unit_price.should be(10.25)
 	end
 
 end
