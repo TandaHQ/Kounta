52f811210340c7d62505eb7d04c8a9357cec49d3
Samuel Richardson <sam@richardson.co.nz>
2013-10-01 11:45:51 +0000

    Remove methods around updating taxes.

--- lib/kounta/tax.rb
+++ lib/kounta/tax.rb
@@ -7,21 +7,6 @@ module Kounta
 		property :code
 		property :name
 		property :rate
-
-		def to_hash
-			{
-				code: code,
-				name: name,
-				rate: rate
-			}
-		end
-
-		private
-
-		def resource_path
-			{companies: company_id, taxes: id}
-		end
-
 	end
 
 end
