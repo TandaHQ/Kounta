431bd5b9e7bb1b30032e0c32ff7fdf08eff73cd5
Samuel Richardson <sam@richardson.co.nz>
2013-10-14 02:42:25 +0000

    Make tests get picked up automatically. Handle errors better.

--- lib/kounta/rest/client.rb
+++ lib/kounta/rest/client.rb
@@ -26,9 +26,9 @@ module Kounta
 			end
 
 			def perform(url_hash, request_method, options={})
-				response = @conn.send(request_method.to_sym, "#{path_from_hash(url_hash)}.#{FORMAT.to_s}", options).body
-				raise Kounta::Errors::APIError, response['error_description'] if response['error']
-				response
+				response = @conn.send(request_method.to_sym, "#{path_from_hash(url_hash)}.#{FORMAT.to_s}", options)
+				raise Kounta::Errors::APIError, response.body['error_description'] if response.status != 200 && response.status != 201
+				response.body
 			end
 
 			def objects_from_response(klass, request_method, url_hash, options={})
