012f3afad078469e586e9c22106b67262941675d
Samuel Richardson <sam@richardson.co.nz>
2013-12-13 09:37:41 +1100

    New field available

--- lib/kounta/product.rb
+++ lib/kounta/product.rb
@@ -13,6 +13,7 @@ module Kounta
 		property :cost_price
 		property :taxes
 		property :sites
+		property :number
 
 		has_many :categories, Kounta::Category, {:company_id => :company_id}, lambda { |klass| {companies: klass.company_id, products: klass.id, categories: nil} }
 		coerce_key :taxes, Kounta::Tax
